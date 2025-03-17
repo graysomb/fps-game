75bf4330386f64e2e0518c52512c4596a25c9d91
graysomb <micharlg2767@gmail.com>
2025-03-16 22:58:12 -0600

    fricken rocks dawg

--- fps_game.c
+++ fps_game.c
@@ -74,6 +74,47 @@ typedef struct BSPNode {
     bool isLeaf;
 } BSPNode;
 
+//generate wall texture
+// A simple integer noise function that returns a value in [-1, 1]
+float noise(int x, int y) {
+    int n = x + y * 57;
+    n = (n << 13) ^ n;
+    return ( 1.0f - ((n * (n * n * 15731 + 789221) + 1376312589) & 0x7fffffff) / 1073741824.0f );
+}
+
+// Interpolated (smoothed) noise
+float smoothNoise(float x, float y) {
+    int xi = (int)x;
+    int yi = (int)y;
+    float fracX = x - xi;
+    float fracY = y - yi;
+    
+    float n00 = noise(xi, yi);
+    float n10 = noise(xi+1, yi);
+    float n01 = noise(xi, yi+1);
+    float n11 = noise(xi+1, yi+1);
+    
+    float i1 = n00 * (1 - fracX) + n10 * fracX;
+    float i2 = n01 * (1 - fracX) + n11 * fracX;
+    return i1 * (1 - fracY) + i2 * fracY;
+}
+
+// Fractal Brownian Motion using multiple octaves of smooth noise
+float fbm(float x, float y) {
+    float total = 0.0f;
+    float amplitude = 1.0f;
+    float frequency = 1.0f;
+    float persistence = 0.5f;
+    int octaves = 4;  // Adjust the number of octaves as desired
+    for (int i = 0; i < octaves; i++) {
+        total += smoothNoise(x * frequency, y * frequency) * amplitude;
+        amplitude *= persistence;
+        frequency *= 2.0f;
+    }
+    return total;
+}
+
+
 // Create a new BSP node
 BSPNode* CreateBSPNode(float x, float z, float width, float depth) {
     BSPNode* node = (BSPNode*)malloc(sizeof(BSPNode));
@@ -288,6 +329,61 @@ void CreateCheckerTexture() {
     // Upload texture data (RGB format)
     glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, TEX_SIZE, TEX_SIZE, 0,
                  GL_RGB, GL_UNSIGNED_BYTE, image);
+}
+
+// --- Create a Rocky Texture ---
+// Texture ID
+GLuint texRock = 1;
+// Generate a procedural rock texture (128x128)
+void CreateRockTexture() {
+    const int TEX_SIZE = 128;
+    unsigned char image[TEX_SIZE * TEX_SIZE * 3];
+    float scale = 8.0f;  // Controls the "zoom" level of the noise
+    
+    // With 4 octaves, the maximum possible amplitude sum is ~1 + 0.5 + 0.25 + 0.125 = 1.875.
+    // We'll normalize fbm() output (which is roughly in [-1.875, 1.875]) to [0,1].
+    float amplitudeSum = 1.875f;
+    
+    // Loop over each pixel
+    for (int j = 0; j < TEX_SIZE; ++j) {
+        for (int i = 0; i < TEX_SIZE; ++i) {
+            // Map pixel coordinates to noise space
+            float x = ((float)i / TEX_SIZE) * scale;
+            float y = ((float)j / TEX_SIZE) * scale;
+            float value = fbm(x, y);
+            // Normalize from roughly [-amplitudeSum, amplitudeSum] to [0,1]
+            value = (value + amplitudeSum) / (2.0f * amplitudeSum);
+            
+            // Map the noise value to a rocky color.
+            // Here we use a base rock color with slight variation.
+            // You can experiment with these formulas to get different hues.
+            int r = (int)(value * 80 + 70 + (rand() % 10));  // A dark, earthy red tone
+            int g = (int)(value * 80 + 70 + (rand() % 10));  // Similar green value for grayish look
+            int b = (int)(value * 80 + 90 + (rand() % 10));  // A touch more blue for a cool rock feel
+
+            // Clamp to [0, 255]
+            if (r > 255) r = 255;
+            if (g > 255) g = 255;
+            if (b > 255) b = 255;
+            
+            image[(j * TEX_SIZE + i) * 3 + 0] = (unsigned char)r;
+            image[(j * TEX_SIZE + i) * 3 + 1] = (unsigned char)g;
+            image[(j * TEX_SIZE + i) * 3 + 2] = (unsigned char)b;
+        }
+    }
+    
+    // Create the OpenGL texture object
+    glGenTextures(1, &texRock);
+    glBindTexture(GL_TEXTURE_2D, texRock);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    // Upload texture data (RGB format)
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, TEX_SIZE, TEX_SIZE, 0,
+                 GL_RGB, GL_UNSIGNED_BYTE, image);
+    
+    // You can store texRock in a global variable for later use in your render loop.
 }
 
 // Utility: Clamp a value between min and max
@@ -628,6 +724,7 @@ int main(int argc, char *argv[]) {
 
     // Create textures (checkerboard)
     CreateCheckerTexture();
+    CreateRockTexture();
 
     // Setup initial game state
     ResetGame();
@@ -845,7 +942,7 @@ int main(int argc, char *argv[]) {
             glEnd();
 
             // Draw obstacles (textured cubes)
-            glBindTexture(GL_TEXTURE_2D, texChecker);
+            glBindTexture(GL_TEXTURE_2D, texRock);
             for (int o = 0; o < NUM_OBSTACLES; ++o) {
                 DrawTexturedBox(obstacles[o]);
             }
@@ -853,7 +950,7 @@ int main(int argc, char *argv[]) {
 
             // Disable texture if you want plain colored BSP elements
             //glDisable(GL_TEXTURE_2D);
-            glBindTexture(GL_TEXTURE_2D, texChecker);
+            glBindTexture(GL_TEXTURE_2D, texRock);
 
             // Draw BSP generated rooms
             DrawBSPRooms(root);
